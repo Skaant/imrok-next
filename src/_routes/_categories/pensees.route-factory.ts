7063aae749397f572766869598e1bb86f106449c
Rimarok <romaric.ruga@gmail.com>
2022-02-09 06:56:57 +0100

    penseesRouteFactory: use getContent for videos

--- src/_routes/_categories/pensees.route-factory.ts
+++ src/_routes/_categories/pensees.route-factory.ts
@@ -1,28 +1,43 @@
 import { CATEGORIES_DATA } from "../../_data/categories.data";
 import CATEGORIES from "../../_enums/categories.enum";
-import CONTENT_TYPES from "../../_enums/content-types.enum";
 import VideoContent from "../../_models/layout/content/_types/Video.content.type";
 import RouteFactory from "../../_models/routes/route-factory.type";
+import getContent from "../../_queries/getContent.query";
 import { DefaultTemplateContext } from "../../_templates/default.template";
 
 const category = CATEGORIES.pensees;
 const { id, title } = CATEGORIES_DATA[category];
 
-const penseesRouteFactory: RouteFactory = (path, createPage) => {
+const penseesRouteFactory: RouteFactory = async (path, createPage, graphql) => {
+  const videos = await getContent<VideoContent>(
+    graphql,
+    `
+      filter: {
+        frontmatter: {
+          type: { eq: "video" },
+        },
+      }
+    `,
+    `
+      frontmatter {
+        id
+        type
+        title
+        tags
+      }
+      body
+    `
+  );
   createPage({
     path: (path += id),
     component: require.resolve("../../_templates/default.template.tsx"),
     context: {
       title,
-      rows: [
-        {
-          content: {
-            type: CONTENT_TYPES.VIDEO,
-            title: "On est pas foutu",
-            id: "Q7SoDK3z4Tg",
-          } as VideoContent,
+      rows: videos.map((content) => ({
+        card: {
+          content,
         },
-      ],
+      })),
     } as DefaultTemplateContext,
   });
 };
