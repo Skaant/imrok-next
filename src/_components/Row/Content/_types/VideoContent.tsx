58792eb105bb58f16b4110f808f4d0ec80ace70f
Rimarok <romaric.ruga@gmail.com>
2022-02-09 06:57:39 +0100

    VideoContent.tsx: add col & w-100

--- src/_components/Row/Content/_types/VideoContent.tsx
+++ src/_components/Row/Content/_types/VideoContent.tsx
@@ -3,8 +3,11 @@ import VideoContentType from "../../../../_models/layout/content/_types/Video.co
 
 function VideoContent({ id, title }: VideoContentType) {
   return (
-    <div className="video-content">
-      <iframe src={`https://youtube.com/watch?v=${id}`}></iframe>
+    <div className="video-content col-md">
+      <iframe
+        src={`https://youtube.com/watch?v=${id}`}
+        className="w-100"
+      ></iframe>
       <p>{title}</p>
     </div>
   );
