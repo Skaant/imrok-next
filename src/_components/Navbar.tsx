b1975ef7102ce8695a1f84188f51f74eb222e62b
Rimarok <romaric.ruga@gmail.com>
2022-02-09 06:55:51 +0100

    styles edit: colors name & use for bg & texts

--- src/_components/Navbar.tsx
+++ src/_components/Navbar.tsx
@@ -7,14 +7,17 @@ function Navbar() {
       id="navbar"
       className="pos-md-sticky w-100 justify-content-center align-items-center direction-column direction-md-row d-flex pt-12 pr-12 pl-12 pr-md-24 pl-md-24"
     >
-      <a href="/" className="h-5 color-light pt-12 pb-12 pl-12 pr-12">
+      <a
+        href="/"
+        className="h-5 color-light pt-12 pb-12 pl-12 pr-12 color-light"
+      >
         IMROK.fr
       </a>
       {Object.entries(CATEGORIES_DATA).map(([category, { title }]) => (
         <a
           href={`/${category}`}
           key={category}
-          className="ml-8 color-secondary pt-12 pb-12 pl-12 pr-12"
+          className="ml-8 color-secondary pt-12 pb-12 pl-12 pr-12 color-highbs"
         >
           {title}
         </a>
