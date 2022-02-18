d3518d0436a966c4f06302726f3b393603be3c88
Rimarok <romaric.ruga@gmail.com>
2022-02-18 22:14:17 +0100

    ProjectContent.type + _data/ 1st project + (1 vid)

--- src/_models/layout/content/_types/Project.content.type.ts
+++ src/_models/layout/content/_types/Project.content.type.ts
@@ -1,10 +1,12 @@
 import CONTENT_TYPES from "../../../../_enums/content-types.enum";
 import ContentCore from "../Content.core.type";
+import Content from "../Content.type";
 
-type VideoContent = ContentCore & {
-  type: CONTENT_TYPES.VIDEO;
+type ProjectContent = ContentCore & {
+  type: CONTENT_TYPES.PROJECT;
   id: string;
   title: string;
+  contents?: Content[];
 };
 
-export default VideoContent;
+export default ProjectContent;
