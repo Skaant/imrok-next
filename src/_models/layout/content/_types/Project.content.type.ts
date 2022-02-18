0f1c318990e63ba431533a31ad8c72057a515511
Rimarok <romaric.ruga@gmail.com>
2022-02-18 23:24:15 +0100

    ProjectContent type rework contentsRef type
    Needed to match querying requirements

--- src/_models/layout/content/_types/Project.content.type.ts
+++ src/_models/layout/content/_types/Project.content.type.ts
@@ -1,12 +1,16 @@
 import CONTENT_TYPES from "../../../../_enums/content-types.enum";
 import ContentCore from "../Content.core.type";
-import Content from "../Content.type";
 
 type ProjectContent = ContentCore & {
   type: CONTENT_TYPES.PROJECT;
   id: string;
   title: string;
-  contents?: Content[];
+  contentsRef?: {
+    /** Used in template provisioning. */
+    key: string;
+    /** Starts in `/_data/`. */
+    path: string;
+  }[];
 };
 
 export default ProjectContent;
