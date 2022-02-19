ac442bb2f564f43cda6c0036fd4c2e45feae35ae
Rimarok <romaric.ruga@gmail.com>
2022-02-19 09:59:57 +0100

    Content type doc: split internal/external contents
    + Add ProjectContent to ExternalContent

--- src/_models/layout/content/Content.type.ts
+++ src/_models/layout/content/Content.type.ts
@@ -1,7 +1,24 @@
 import { ReactNode } from "react";
 import ImageContent from "./_types/Image.content.type";
+import ProjectContent from "./_types/Project.content.type";
 import VideoContent from "./_types/Video.content.type";
 
-type Content = string | ReactNode | ImageContent | VideoContent;
+/** Hard-code contents */
+type InternalContent = string | ReactNode;
+/** Queried content */
+type ExternalContent = ImageContent | VideoContent | ProjectContent;
+
+/**
+ * Pages are made of `Content[]`.
+ *
+ * * *Internal* content is hard-coded in `/src` (`string`, TSX),
+ * * *External* content is described in `/_data` (MDX).
+ *
+ * `Content` will be rendered as `Row`(s), or even `Card`(s).
+ *  *Thus, they should provide optional component configuration.*
+ *
+ * @note Internal/external in the sense of code ?
+ */
+type Content = InternalContent | ExternalContent;
 
 export default Content;
