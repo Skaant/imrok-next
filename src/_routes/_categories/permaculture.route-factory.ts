7da764c544b742198d69537195109395b83bbfea
Rimarok <romaric.ruga@gmail.com>
2022-02-19 21:02:27 +0100

    permaculture.rf: add projects links list

--- src/_routes/_categories/permaculture.route-factory.ts
+++ src/_routes/_categories/permaculture.route-factory.ts
@@ -1,33 +1,45 @@
 import { CATEGORIES_DATA } from "../../_data/categories.data";
 import CATEGORIES from "../../_enums/categories.enum";
+import CONTENT_TYPES from "../../_enums/content-types.enum";
 import RouteFactory from "../../_models/routes/route-factory.type";
+import LinksListContent from "../../_models/layout/content/_types/LinksList.content.type";
 import getProjects, {
   PROJECTS_FILTERS,
 } from "../../_queries/getProjects/getProjects.query";
 import { DefaultTemplateContext } from "../../_templates/default.template";
+import Row from "../../_models/layout/Row.type";
+import COLORS from "../../_enums/colors.enum";
 
 const category = CATEGORIES.permaculture;
 const { id, title } = CATEGORIES_DATA[category];
 
 const permacultureRouteFactory: RouteFactory = async (
-  path,
+  _path,
   createPage,
   graphql
 ) => {
   const projects = await getProjects(graphql, {
     [PROJECTS_FILTERS.PATH]: `_projects/${id}`,
   });
-  console.log("projects", projects);
+  const path = _path + id;
   createPage({
-    path: path + id,
+    path,
     component: require.resolve("../../_templates/default.template.tsx"),
     context: {
       title,
       rows: [
         {
-          content: projects
-            .map((project) => JSON.stringify(project))
-            .join("\n"),
+          content: {
+            id: `${id}-projects`,
+            type: CONTENT_TYPES.LINKS_LIST,
+            title: "Mes jardins",
+            level: 2,
+            color: COLORS.dark,
+            links: projects.map(({ slug, title }) => ({
+              url: `${path}/${slug}`,
+              label: title,
+            })),
+          } as LinksListContent & Row,
         },
       ],
     } as DefaultTemplateContext,
