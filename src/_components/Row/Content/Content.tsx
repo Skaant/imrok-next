ad74cf3dcb8124c998b61f9b6f323003bec3b15f
Rimarok <romaric.ruga@gmail.com>
2022-02-19 20:56:56 +0100

    <Content/> props union Row + add LinksListContent

--- src/_components/Row/Content/Content.tsx
+++ src/_components/Row/Content/Content.tsx
@@ -4,20 +4,29 @@ import ContentType from "../../../_models/layout/content/Content.type";
 import ImageContentType from "../../../_models/layout/content/_types/Image.content.type";
 import VideoContentType from "../../../_models/layout/content/_types/Video.content.type";
 import ImageContent from "./_types/ImageContent";
+import LinksListContent from "./_types/LinksListContent";
 import VideoContent from "./_types/VideoContent";
+import LinksListContentType from "../../../_models/layout/content/_types/LinksList.content.type";
+import Row from "../../../_models/layout/Row.type";
 
 /**
  * `<Content {...content} />` is purely a **switch component**.
  */
-function Content({ content }: { content: ContentType }) {
-  if (typeof content === "string" || typeof content === "function")
-    return <>{content}</>;
+function Content({ content }: { content: ContentType & Row }) {
+  if (typeof content === "string") return <>{content}</>;
   if (typeof content === "object") {
+    if (content["$$typeof"] === Symbol.for("react.element")) {
+      return content;
+    }
     switch (content["type"]) {
       case CONTENT_TYPES.IMAGE:
         return <ImageContent {...(content as ImageContentType)} />;
       case CONTENT_TYPES.VIDEO:
         return <VideoContent {...(content as VideoContentType)} />;
+      case CONTENT_TYPES.LINKS_LIST:
+        return (
+          <LinksListContent {...(content as LinksListContentType & Row)} />
+        );
       default:
         throw Error(
           `Content type not valid : ${JSON.stringify(
--- src/_enums/content-types.enum.ts
+++ src/_enums/content-types.enum.ts
@@ -2,8 +2,13 @@
  * @note Types `string` & `ReactElement` are implicity included.
  */
 enum CONTENT_TYPES {
+  /** Internal */
+  LINKS_LIST = "LINKS_LIST",
+  /** External */
   IMAGE = "image",
+  /** External */
   VIDEO = "video",
+  /** External */
   PROJECT = "project",
 }
 
