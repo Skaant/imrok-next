d063e3726884784c411874cf07e08d67a16adcce
Rimarok <romaric.ruga@gmail.com>
2022-02-19 09:54:37 +0100

    ProjectContent add refs & contents props
    + refs integration in demo project

--- src/_models/layout/content/_types/Project.content.type.ts
+++ src/_models/layout/content/_types/Project.content.type.ts
@@ -1,16 +1,32 @@
 import CONTENT_TYPES from "../../../../_enums/content-types.enum";
 import ContentCore from "../Content.core.type";
+import Content from "../Content.type";
+
+/** Used to query sub-contents. */
+type ContentRef = {
+  /** Used for template provisioning. */
+  key: string;
+  /** Starts in `/_data/`. */
+  path: string;
+};
 
 type ProjectContent = ContentCore & {
   type: CONTENT_TYPES.PROJECT;
   id: string;
   title: string;
-  contentsRef?: {
-    /** Used in template provisioning. */
-    key: string;
-    /** Starts in `/_data/`. */
-    path: string;
-  }[];
+  /**
+   * `refs` hold others contents links, which will be
+   *  queried to provision `contents` prop.
+   */
+  refs?: ContentRef[];
+  /**
+   * `contents` is provisioned from `refs` by
+   *  specific queries like `getProjects`.
+   *
+   * If not processed through these methods,
+   *  `contents` will be left `undefined`.
+   */
+  contents?: Content[];
 };
 
 export default ProjectContent;
