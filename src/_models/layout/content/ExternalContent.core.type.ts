4a55b11b5dc01ac22daaa6bc43d6a2c687882dd3
Rimarok <romaric.ruga@gmail.com>
2022-02-20 14:40:45 +0100

    ExternalContent: add body? prop
    From ContentCore

--- src/_models/layout/content/ExternalContent.core.type.ts
+++ src/_models/layout/content/ExternalContent.core.type.ts
@@ -1,7 +1,18 @@
 import ContentCore from "./Content.core.type";
 
+/**
+ * Dynamic content, queried from
+ *  `_data` folder through GraphQL.
+ */
 type ExternalContentCore = ContentCore & {
   _id: string;
+  /**
+   * This particular field will be extracted from
+   *  the MDX part of the file, while all others
+   *  properties will be extracted from
+   *  the frontammer YAML.
+   */
+  body?: string;
 };
 
 export default ExternalContentCore;
