ebb4bea0c70675d4708a0ffa31359d9fe75eb3ad
Rimarok <romaric.ruga@gmail.com>
2022-02-20 14:40:21 +0100

    Content: extract str & ReactElement from Internal

--- src/_models/layout/content/Content.type.ts
+++ src/_models/layout/content/Content.type.ts
@@ -1,26 +1,32 @@
 import { ReactElement } from "react";
+import Row from "../Row.type";
 import ImageContent from "./_types/Image.content.type";
 import LinksListContent from "./_types/LinksList.content.type";
 import ProjectContent from "./_types/Project.content.type";
 import VideoContent from "./_types/Video.content.type";
 
 /** Hard-code contents */
-export type InternalContent = string | ReactElement | LinksListContent;
+export type InternalContent = LinksListContent;
 /** Queried content */
 export type ExternalContent = ImageContent | VideoContent | ProjectContent;
 
 /**
  * Pages are made of `Content[]`.
  *
- * * *Internal* content is hard-coded in `/src` (`string`, TSX),
+ * * *Internal* content *along with `string` and TSX* is
+ *   hard-coded in `/src`,
  * * *External* content is queried with graphql from `/_data` (MDX).
  *
  * `Content` will be rendered as `Row`(s), or even `Card`(s).
- *  *Thus,* external content *could provide optional
- *  component configuration.*
+ *
+ *  Thus, **both internal & external contents provide `Row` configuration**.
  *
  * @note Internal/external in the sense of code ?
  */
-type Content = InternalContent | ExternalContent;
+type Content =
+  | string
+  | ReactElement
+  | Row<InternalContent>
+  | Row<ExternalContent>;
 
 export default Content;
