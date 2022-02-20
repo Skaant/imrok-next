ecfa4b914e357906e08176fd568d3cd2db960ab6
Rimarok <romaric.ruga@gmail.com>
2022-02-20 14:38:33 +0100

    Section type: open for level 1

--- src/_models/layout/Section.type.ts
+++ src/_models/layout/Section.type.ts
@@ -3,7 +3,7 @@ import Content from "./content/Content.type";
 type Section = {
   id?: string;
   title?: string;
-  level?: 2 | 3 | 4 | 5 | 6;
+  level?: 1 | 2 | 3 | 4 | 5 | 6;
   content?: Content;
   background?: string;
   color?: string;
