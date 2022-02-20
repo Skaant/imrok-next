2aad1c4b15bcb4c93c711f8925f6068fc5bf7aa1
Rimarok <romaric.ruga@gmail.com>
2022-02-20 15:08:44 +0100

    CONTENT_TYPES: add planned future types

--- src/_enums/content-types.enum.ts
+++ src/_enums/content-types.enum.ts
@@ -2,14 +2,20 @@
  * @note Types `string` & `ReactElement` are implicity included.
  */
 enum CONTENT_TYPES {
+  /** External */
+  TEXT = "TEXT",
+  /** External */
+  IMAGE = "IMAGE",
+  /** External */
+  GALLERY = "GALLERY",
+  /** External */
+  VIDEO = "VIDEO",
+  /** External */
+  PROJECT = "PROJECT",
   /** Internal */
   LINKS_LIST = "LINKS_LIST",
-  /** External */
-  IMAGE = "image",
-  /** External */
-  VIDEO = "video",
-  /** External */
-  PROJECT = "project",
+  /** Internal */
+  CTA = "CTA",
 }
 
 export default CONTENT_TYPES;
