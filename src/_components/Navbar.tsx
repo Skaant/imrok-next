a4297648169a3badd89367d5eb3340db8a7a64dc
Rimarok <romaric.ruga@gmail.com>
2022-02-20 08:51:14 +0100

    navbar: greatly improve style + mobile behavior

--- src/_components/Navbar.tsx
+++ src/_components/Navbar.tsx
@@ -7,17 +7,23 @@ function Navbar() {
       id="navbar"
       className="pos-md-sticky w-100 justify-content-center align-items-center direction-column direction-md-row d-flex pt-12 pr-12 pl-12 pr-md-24 pl-md-24"
     >
-      <a
-        href="/"
-        className="h-5 color-light pt-12 pb-12 pl-12 pr-12 color-light"
-      >
-        IMROK.fr
-      </a>
+      <div className="h5 p-16 p-md-24 pr-md-0">
+        <a id="navbar-home" href="/" className="h-5 p-12 color-light">
+          IMROK.fr
+        </a>
+        <span className="p-16 color-light">·</span>
+        <button
+          id="navbar-toggle"
+          className="d-md-none p-16 bg-unset color-light text-uppercase bo-none"
+        >
+          Menu ▼
+        </button>
+      </div>
       {Object.entries(CATEGORIES_DATA).map(([category, { title }]) => (
         <a
           href={`/${category}`}
           key={category}
-          className="ml-8 color-secondary pt-12 pb-12 pl-12 pr-12 color-highbs"
+          className="navbar-category ml-8 color-highbs"
         >
           {title}
         </a>
