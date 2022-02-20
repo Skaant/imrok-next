2b3941b3d05869aac237746ee1717875282142e7
Rimarok <romaric.ruga@gmail.com>
2022-02-20 06:52:46 +0100

    LinkListContent: add type prop

--- src/_models/layout/content/_types/LinksList.content.type.ts
+++ src/_models/layout/content/_types/LinksList.content.type.ts
@@ -1,6 +1,8 @@
+import CONTENT_TYPES from "../../../../_enums/content-types.enum";
 import { Link } from "../../../Link.type";
 
 type LinksListContent = {
+  type: CONTENT_TYPES.LINKS_LIST;
   id: string;
   title: string;
   description?: string;
