db8636e3ac7bad57c48862aa820b6b831913e493
Rimarok <romaric.ruga@gmail.com>
2022-02-21 21:35:48 +0100

    GET_CONTENT_FILTERS enum: from PROJECT_FILTERS

---
+++ src/_queries/getContents/_enums/getContentFilters.enum.ts
@@ -0,0 +1,9 @@
+enum GET_CONTENT_FILTERS {
+  /**
+   * Starts in `/_data/` folder, thus
+   *  **given `path` should not start with a dash**.
+   */
+  PATH = "path",
+}
+
+export default GET_CONTENT_FILTERS;
