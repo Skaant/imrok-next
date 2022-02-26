fcddb692de8e5dee398a4aac5656e81fb8fc0f8d
Rimarok <romaric.ruga@gmail.com>
2022-02-26 16:43:45 +0100

    getContentTypes & Filters + specs Row & Text type

--- src/_queries/getContents/getContents.query.spec.ts
+++ src/_queries/getContents/getContents.query.spec.ts
@@ -1,50 +1,76 @@
+import CONTENT_TYPES from "../../_enums/content-types.enum";
 import DataAllMdx from "../../_helpers/models/dataAllMdx.model";
-import NodeItemCore from "../../_types/queries/node-item-core.model";
+import TextContent from "../../_types/content/_externalContents/TextContent.type";
+import NodeItemCore from "../../_types/queries/NodeItemCore.model";
 import getContents from "./getContents.query";
+import * as argsBuilder from "./_helpers/argsBuilder.helper";
+import * as fieldsBuilder from "./_helpers/fieldsBuilder.helper";
 
 describe("getContents query", () => {
-  type MockContent = { body: string; title: string };
-  const graphqlSpy = jest.fn().mockResolvedValue({
-    data: {
-      allMdx: {
-        nodes: [
-          {
-            frontmatter: {
-              title: "title",
+  let graphqlSpy: jest.Mock;
+
+  beforeEach(() => {
+    graphqlSpy = jest.fn().mockResolvedValue({
+      data: {
+        allMdx: {
+          nodes: [
+            {
+              frontmatter: {
+                _id: "yodeleihihou",
+                type: CONTENT_TYPES.TEXT,
+              },
+              body: "body",
             },
-            body: "body",
-          },
-        ],
-      },
-    } as DataAllMdx<NodeItemCore<Omit<MockContent, "body">>>,
+          ],
+        },
+      } as DataAllMdx<NodeItemCore<Omit<TextContent, "body">>>,
+    });
   });
 
-  it("Should call graphql with given props and query", async () => {
-    await getContents<MockContent>(graphqlSpy, "query", "params");
+  it("Should call both argsBuilder and fieldsBuilder", async () => {
+    const argsBuilderSpy = jest.spyOn(argsBuilder, "default");
+    const fieldsBuilderSpy = jest.spyOn(fieldsBuilder, "default");
+    await getContents(graphqlSpy, {});
+    expect(argsBuilderSpy).toHaveBeenCalled();
+    expect(fieldsBuilderSpy).toHaveBeenCalled();
+  });
+
+  it("Should call graphql with default args and fields", async () => {
+    await getContents(graphqlSpy, {});
     // Respect indentation
     expect(graphqlSpy).toHaveBeenCalledWith(`
     query {
-      allMdx(
-        params
-      ) {
+      allMdx {
         nodes {
-          query
+          frontmatter {
+            _id
+            alt
+            category
+            id
+            legend
+            refs { key _id }
+            slug
+            tags
+            title
+            type
+            url
+          }
+          body
         }
       }
     }`);
   });
 
-  it("Should reformat graphql result to ContentType format", async () => {
-    const result = await getContents<MockContent>(
-      graphqlSpy,
-      "query",
-      "params"
-    );
+  it("Should reformat graphql result to TextContent format", async () => {
+    const result = (await getContents(graphqlSpy, {
+      types: [CONTENT_TYPES.TEXT],
+    })) as TextContent[];
     expect(result).toEqual([
       {
-        title: "title",
+        _id: "yodeleihihou",
         body: "body",
-      },
+        type: CONTENT_TYPES.TEXT,
+      } as TextContent,
     ]);
   });
 
--- src/_queries/getContents/getContents.query.ts
+++ src/_queries/getContents/getContents.query.ts
@@ -1,28 +1,51 @@
 import { CreatePagesArgs } from "gatsby";
 import DataAllMdx from "../../_helpers/models/dataAllMdx.model";
-import ExternalContentCore from "../../_types/content/ExternalContentCore.type";
+import ExternalContent from "../../_types/content/ExternalContent.type";
+import ExternalContentTypes from "../../_types/content/ExternalContentTypes.type";
+import Row from "../../_types/layout/Row.type";
 import NodeItemCore from "../../_types/queries/NodeItemCore.model";
+import GET_CONTENT_FILTERS from "./_enums/getContentFilters.enum";
+import argsBuilder from "./_helpers/argsBuilder.helper";
+import fieldsBuilder from "./_helpers/fieldsBuilder.helper";
+
+export type GetContentsTypes =
+  | ExternalContentTypes
+  | ExternalContentTypes[]
+  | Row<ExternalContentTypes>
+  | Row<ExternalContentTypes>[];
+
+export type GetContentsFilters = { [key in GET_CONTENT_FILTERS]?: string };
 
 /**
  * Genericly typed content query.
  */
-async function getContents<ContentType extends ExternalContentCore>(
+async function getContents(
   graphql: CreatePagesArgs["graphql"],
-  /** Starts at `query { allMdx() { nodes {`. */
-  format: string,
-  params?: string
-): Promise<ContentType[]> {
+  {
+    types,
+    filters,
+  }: {
+    types?: GetContentsTypes;
+    filters?: GetContentsFilters;
+  } = {},
+  /** Add `Row` type props to query & results. */
+  rows?: true
+): Promise<ExternalContent[]> {
+  console.log(`
+    query {
+      allMdx${argsBuilder({ types, filters })} {
+        nodes {
+          ${fieldsBuilder({ types }, rows)}
+          body
+        }
+      }
+    }`);
   const result = await graphql(`
     query {
-      allMdx${
-        params
-          ? `(
-        ${params}
-      )`
-          : ""
-      } {
+      allMdx${argsBuilder({ types, filters })} {
         nodes {
-          ${format}
+          ${fieldsBuilder({ types }, rows)}
+          body
         }
       }
     }`);
@@ -30,11 +53,11 @@ async function getContents<ContentType extends ExternalContentCore>(
     throw new Error(result.errors);
   }
   return (
-    result.data as DataAllMdx<NodeItemCore<Omit<ContentType, "body">>>
+    result.data as DataAllMdx<NodeItemCore<Omit<ExternalContent, "body">>>
   ).allMdx.nodes.map(({ frontmatter, body }) => ({
     ...frontmatter,
     body,
-  })) as ContentType[];
+  })) as ExternalContent[];
 }
 
 export default getContents;
