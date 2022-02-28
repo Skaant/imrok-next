98890802541dc4006cb64abcb52892a7de5f2569
Rimarok <romaric.ruga@gmail.com>
2022-02-28 05:45:50 +0100

    LinksListContent: links prop hold some cont props

--- src/_types/content/_internalContents/LinksListContent.type.ts
+++ src/_types/content/_internalContents/LinksListContent.type.ts
@@ -1,12 +1,19 @@
+import CATEGORIES from "../../../_enums/categories.enum";
 import CONTENT_TYPES from "../../../_enums/content-types.enum";
 import { Link } from "../../Link.type";
+import ContentCore from "../ContentCore.type";
 
-type LinksListContent = {
+type LinksListContent = ContentCore & {
   type: CONTENT_TYPES.LINKS_LIST;
   id: string;
   title: string;
   description?: string;
-  links: Link[];
+  links: (Link & {
+    date?: string;
+    category?: CATEGORIES;
+    tags?: string[];
+    type?: CONTENT_TYPES;
+  })[];
 };
 
 export default LinksListContent;
