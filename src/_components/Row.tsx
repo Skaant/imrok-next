591148a7d0ba61dd652cd7dbecfa7fcda757c620
Rimarok <romaric.ruga@gmail.com>
2022-02-28 08:18:52 +0100

    Row: open to received children

--- src/_components/Row.tsx
+++ src/_components/Row.tsx
@@ -1,8 +1,30 @@
 import * as React from "react";
+import { ReactElement } from "react-markdown/lib/react-markdown";
 import Content from "../_types/content/Content.type";
 import RowType from "../_types/layout/Row.type";
 import Card from "./Card";
 import ContentSwitch from "./ContentSwitch";
+
+function RowContainer({
+  id,
+  background,
+  color,
+  className,
+  children,
+}: Pick<RowType, "id" | "background" | "color" | "className"> & {
+  children: ReactElement;
+}) {
+  return (
+    <div
+      {...(id && { id })}
+      className={`row p-12 p-md-24 shadow ${
+        background ? ` bg-${background}` : ""
+      }${color ? ` color-${color}` : ""}${className ? ` ${className}` : ""}`}
+    >
+      {children}
+    </div>
+  );
+}
 
 function Row({
   id,
@@ -13,32 +35,48 @@ function Row({
   color,
   card,
   className,
+  children,
   ...props
-}: RowType) {
-  const _content = !content
-    ? Object.keys(props).length
-      ? (props as Content)
-      : undefined
-    : typeof content === "object"
-    ? { ...content, ...(Object.keys(props).length ? props : {}) }
-    : content;
-  return (
-    <div
-      {...(id && { id })}
-      className={`row p-12 p-md-24 shadow ${
-        background ? ` bg-${background}` : ""
-      }${color ? ` color-${color}` : ""}${className ? ` ${className}` : ""}`}
-    >
-      {card ? (
-        <Card {...card} />
-      ) : (
+}: RowType & {
+  children?: ReactElement;
+}) {
+  if (children) {
+    return (
+      <RowContainer
+        id={id}
+        background={background}
+        color={color}
+        className={className}
+      >
+        {children}
+      </RowContainer>
+    );
+  } else {
+    const _content = !content
+      ? Object.keys(props).length
+        ? (props as Content)
+        : undefined
+      : typeof content === "object"
+      ? { ...content, ...(Object.keys(props).length ? props : {}) }
+      : content;
+    return (
+      <RowContainer
+        id={id}
+        background={background}
+        color={color}
+        className={className}
+      >
         <>
           {level && title && React.createElement(`h${level}`, {}, title)}
-          {_content && <ContentSwitch content={_content} />}
+          {card ? (
+            <Card {...card} />
+          ) : (
+            content && <ContentSwitch content={_content} />
+          )}
         </>
-      )}
-    </div>
-  );
+      </RowContainer>
+    );
+  }
 }
 
 export default Row;
