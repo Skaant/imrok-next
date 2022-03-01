97ce9819760f3d8f26acf08db7568a67b368159b
Rimarok <romaric.ruga@gmail.com>
2022-03-01 18:59:54 +0100

    home: mv news fetch from route to page

--- src/_routes/home.page.ts
+++ src/_routes/home.page.ts
@@ -1,17 +1,21 @@
 import COLORS from "../_enums/colors.enum";
 import CONTENT_TYPES from "../_enums/content-types.enum";
+import getContents from "../_queries/getContents/getContents.query";
+import GET_CONTENT_SORTS from "../_queries/getContents/_enums/getContentSorts.enum";
 import { HomeTemplateContext } from "../_templates/home.template";
 import ExternalContent from "../_types/content/ExternalContent.type";
 import ImageContent from "../_types/content/_externalContents/ImageContent.type";
 import LinksListContent from "../_types/content/_internalContents/LinksListContent.type";
 import PageContextFactory from "../_types/routes/PageContextFactory.type";
 
-const homePageContextFactory: PageContextFactory<HomeTemplateContext> = ({
-  news,
-}: {
-  news: ExternalContent[];
-}) =>
-  ({
+const homePageContextFactory: PageContextFactory<HomeTemplateContext> = async (
+  _,
+  graphql
+) => {
+  const news = await getContents(graphql, {
+    sort: { [GET_CONTENT_SORTS.UPDATED_AT]: "DESC" },
+  });
+  return {
     title: "IMROK",
     subtitle: "Le hub créatif de Romaric Ruga",
     rows: [
@@ -62,6 +66,7 @@ const homePageContextFactory: PageContextFactory<HomeTemplateContext> = ({
         } as ImageContent,
       },
     ],
-  } as HomeTemplateContext);
+  } as HomeTemplateContext;
+};
 
 export default homePageContextFactory;
--- src/_routes/home.route.ts
+++ src/_routes/home.route.ts
@@ -1,16 +1,11 @@
 import RouteFactory from "../_types/routes/RouteFactory.type";
-import getContents from "../_queries/getContents/getContents.query";
-import GET_CONTENT_SORTS from "../_queries/getContents/_enums/getContentSorts.enum";
 import homePageContextFactory from "./home.page";
 
 const homeRouteFactory: RouteFactory = async (path, createPage, graphql) => {
-  const news = await getContents(graphql, {
-    sort: { [GET_CONTENT_SORTS.UPDATED_AT]: "DESC" },
-  });
   createPage({
     path,
     component: require.resolve("../_templates/home.template.tsx"),
-    context: homePageContextFactory({ news }),
+    context: await homePageContextFactory(null, graphql),
   });
 };
 
