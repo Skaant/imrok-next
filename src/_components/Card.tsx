c0c59c6f9765ac4628fde1e5845b5b7115e3d421
Rimarok <romaric.ruga@gmail.com>
2022-03-02 21:38:20 +0100

    Card comp: harmo params on Section + ContentSwitch

--- src/_components/Card.tsx
+++ src/_components/Card.tsx
@@ -1,25 +1,31 @@
 import * as React from "react";
 import COLORS from "../_enums/colors.enum";
-import CardType from "../_models/layout/Card.type";
-import Content from "./ContentSwitch";
+import Content from "../_types/content/Content.type";
+import Section from "../_types/layout/Section.type";
+import ContentSwitch from "./ContentSwitch";
 
-function Card({
-  background = COLORS.light,
-  color = COLORS.dark,
-  level,
-  title,
-  content,
-  col = "md",
-  className,
-}: CardType) {
+function Card(
+  props: Section<Content> & {
+    children?: React.ReactElement;
+  }
+) {
+  const {
+    background = COLORS.light,
+    color = COLORS.dark,
+    level,
+    title,
+    col = "md",
+    className,
+    children,
+  } = props;
   return (
     <div
       className={`card p-24 p-md-32 bo-rad-6 shadow-m bg-${background} color-${color}${` col-${col}`}${
         className ? ` ${className}` : ""
       }`}
     >
-      {level && title && React.createElement(`h${level}`, {}, title)}
-      <Content content={content} />
+      {title && level && React.createElement(`h${level}`, {}, title)}
+      {children ? children : <ContentSwitch target="card" {...props} />}
     </div>
   );
 }
