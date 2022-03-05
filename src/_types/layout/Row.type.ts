0662d94db4bf4a4067f78b5c504bbddaacbd2570
Rimarok <romaric.ruga@gmail.com>
2022-03-05 10:03:50 +0100

    Row type: CardContentType undefined by default

--- src/_types/layout/Row.type.ts
+++ src/_types/layout/Row.type.ts
@@ -1,10 +1,20 @@
 import Section, { SectionContent, SectionFields } from "./Section.type";
 
+/**
+ * `Row` cannot hold both `card` and
+ *  `content` props.
+ *
+ * In component, card will be rendered in
+ *  place of `content` if given.
+ *
+ * That's partly why `CardContentType` is
+ *  `undefined` by default.
+ */
 type Row<
   RowContentType extends SectionContent = SectionContent,
-  ColContentType extends SectionContent = SectionContent
+  CardContentType extends SectionContent = undefined
 > = Section<RowContentType> & {
-  card?: Section<ColContentType>;
+  card?: Section<CardContentType>;
 };
 
 export default Row;
