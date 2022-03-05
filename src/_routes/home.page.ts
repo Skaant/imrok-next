c20981ee6aa4c7257d334c5155fb3e4f4632a774
Rimarok <romaric.ruga@gmail.com>
2022-03-05 09:54:12 +0100

    home page: add highlight & fix links list

--- src/_routes/home.page.ts
+++ src/_routes/home.page.ts
@@ -1,17 +1,24 @@
 import COLORS from "../_enums/colors.enum";
 import CONTENT_TYPES from "../_enums/content-types.enum";
 import getContents from "../_queries/getContents/getContents.query";
+import GET_CONTENT_FILTERS from "../_queries/getContents/_enums/getContentFilters.enum";
 import GET_CONTENT_SORTS from "../_queries/getContents/_enums/getContentSorts.enum";
 import { HomeTemplateContext } from "../_templates/home.template";
-import ExternalContent from "../_types/content/ExternalContent.type";
 import ImageContent from "../_types/content/_externalContents/ImageContent.type";
 import LinksListContent from "../_types/content/_internalContents/LinksListContent.type";
 import PageContextFactory from "../_types/routes/PageContextFactory.type";
+import Row from "../_types/layout/Row.type";
+import Content from "../_types/content/Content.type";
 
 const homePageContextFactory: PageContextFactory<HomeTemplateContext> = async (
   _,
   graphql
 ) => {
+  const highlight = (
+    await getContents(graphql, {
+      filters: { [GET_CONTENT_FILTERS._ID]: "text_l-imagination-est-la-voie" },
+    })
+  )[0];
   const news = await getContents(graphql, {
     sort: { [GET_CONTENT_SORTS.UPDATED_AT]: "DESC" },
   });
@@ -23,10 +30,10 @@ const homePageContextFactory: PageContextFactory<HomeTemplateContext> = async (
         title: "Pensée du moment",
         level: 2,
         card: {
-          content: "L'imagination est la voie !",
+          content: highlight.content,
           col: "md",
         },
-      },
+      } as Row<undefined, Content>,
       {
         background: COLORS.light,
         card: {
@@ -45,20 +52,26 @@ const homePageContextFactory: PageContextFactory<HomeTemplateContext> = async (
       },
       {
         level: 2,
-        type: CONTENT_TYPES.LINKS_LIST,
         id: "global-news",
         title: "Activités récentes",
-        links: news
-          .filter(({ slug }) => slug)
-          .map(({ slug, title, type, category, updatedAt, tags }) => ({
-            url: `${category ? `/${category}` : `/${type}`}/${slug}`,
-            label: title,
-            date: updatedAt,
-            category,
-            tags,
-            type,
-          })),
-      } as LinksListContent,
+        content: {
+          type: CONTENT_TYPES.LINKS_LIST,
+          links: news
+            .filter(({ content: { slug } }) => slug)
+            .map(
+              ({
+                content: { slug, title, type, category, updatedAt, tags },
+              }) => ({
+                url: `${category ? `/${category}` : `/${type}`}/${slug}`,
+                label: title,
+                date: updatedAt,
+                category,
+                tags,
+                type,
+              })
+            ),
+        },
+      } as Row<LinksListContent>,
       {
         content: {
           type: CONTENT_TYPES.IMAGE,
