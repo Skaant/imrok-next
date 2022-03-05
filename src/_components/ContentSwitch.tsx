7427b1956d6698bd0c1c525f2abe48cb0a21a373
Rimarok <romaric.ruga@gmail.com>
2022-03-05 09:43:41 +0100

    ContentSwitch: merge ContentRowSwitch
    Content type components also merged Row & Card display

--- src/_components/ContentSwitch.tsx
+++ src/_components/ContentSwitch.tsx
@@ -1,29 +1,65 @@
 import React, { ReactElement } from "react";
+import CONTENT_TYPES from "../_enums/content-types.enum";
 import ContentType from "../_types/content/Content.type";
-import ExternalContent from "../_types/content/ExternalContent.type";
-import InternalContent from "../_types/content/InternalContent.type";
-import Row from "../_types/layout/Row.type";
-import ContentRowSwitch from "./ContentRowSwitch";
+import Section from "../_types/layout/Section.type";
+import TextContent from "./_contents/TextContent";
+import TextContentType from "../_types/content/_externalContents/TextContent.type";
+import ImageContent from "./_contents/ImageContent";
+import ImageContentType from "../_types/content/_externalContents/ImageContent.type";
+import VideoContent from "./_contents/VideoContent";
+import VideoContentType from "../_types/content/_externalContents/VideoContent.type";
+import LinksListContent from "./_contents/LinksListContent";
+import LinksListContentType from "../_types/content/_internalContents/LinksListContent.type";
+import { RowOrCard } from "./RowOrCardLayout";
 
 function ContentSwitch({
-  content,
-  ...props
-}: {
-  content: ContentType;
-  [key: string]: any;
+  target,
+  ...section
+}: Section<ContentType> & {
+  target: RowOrCard;
 }): ReactElement {
+  const { content } = section;
   if (typeof content === "string") return <>{content}</>;
   if (typeof content === "object") {
     if (content["$$typeof"] === Symbol.for("react.element")) {
       return content as ReactElement;
     }
-    return (
-      <ContentRowSwitch
-        content={
-          { ...content, ...props } as Row<InternalContent | ExternalContent>
-        }
-      />
-    );
+    switch (content["type"]) {
+      case CONTENT_TYPES.TEXT:
+        return (
+          <TextContent
+            target={target}
+            {...(section as Section<TextContentType>)}
+          />
+        );
+      case CONTENT_TYPES.IMAGE:
+        return (
+          <ImageContent
+            target={target}
+            {...(section as Section<ImageContentType>)}
+          />
+        );
+      case CONTENT_TYPES.VIDEO:
+        return (
+          <VideoContent
+            target={target}
+            {...(section as Section<VideoContentType>)}
+          />
+        );
+      case CONTENT_TYPES.LINKS_LIST:
+        return (
+          <LinksListContent
+            target={target}
+            {...(section as Section<LinksListContentType>)}
+          />
+        );
+      default:
+        throw Error(
+          `Content type not valid : ${JSON.stringify(
+            content
+          )} ${typeof content} ${content["type"]} (${typeof content["type"]})`
+        );
+    }
   }
   throw Error(`Content is not valid : ${content} ${typeof content})`);
 }
