bf40268044903cae7d7c2f65ad170267adcce824
Rimarok <romaric.ruga@gmail.com>
2022-03-16 22:03:10 +0100

    LinksListContent type: add color to overide link

--- src/_types/content/_internalContents/LinksListContent.type.ts
+++ src/_types/content/_internalContents/LinksListContent.type.ts
@@ -1,4 +1,5 @@
 import CATEGORIES from "../../../_enums/categories.enum";
+import { BaseTextColors } from "../../../_enums/colors.enum";
 import CONTENT_TYPES from "../../../_enums/content-types.enum";
 import { Link } from "../../Link.type";
 import ContentCore from "../ContentCore.type";
@@ -7,6 +8,8 @@ type LinksListContent = ContentCore & {
   type: CONTENT_TYPES.LINKS_LIST;
   id?: string;
   description?: string;
+  /** Overrides `<a/>` link color. */
+  color?: BaseTextColors;
   links: (Link & {
     date?: string;
     category?: CATEGORIES;
