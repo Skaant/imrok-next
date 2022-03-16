b68a274a7534dfa7eb32fda68543f8f1c8f8c089
Rimarok <romaric.ruga@gmail.com>
2022-03-16 22:05:24 +0100

    home.template:  add font-curvy to subtitle

--- src/_templates/home.template.tsx
+++ src/_templates/home.template.tsx
@@ -20,7 +20,9 @@ function HomeTemplate({ pageContext }: PageProps<any, HomeTemplateContext>) {
       <Row id={`${pageId}-title`} className="text-center color-light">
         <>
           <h1 className="title-7 hol-adin mb-0">{title}</h1>
-          <p className="paragraph-2 mb-36 mb-md-48">{subtitle}</p>
+          <p className="font-curvy display-4 mb-36 mb-md-48 color-light-o-75">
+            {subtitle}
+          </p>
         </>
       </Row>
       {rows && rows.map((row, index) => <Row key={index} {...row} />)}
