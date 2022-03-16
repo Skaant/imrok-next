299b6fa7eedb4b8e560aaa90285ce3bb43eba026
Rimarok <romaric.ruga@gmail.com>
2022-03-16 22:07:16 +0100

    colors: add light & dark opacity variations

--- src/_enums/colors.enum.ts
+++ src/_enums/colors.enum.ts
@@ -1,8 +1,15 @@
 enum COLORS {
   dark = "dark",
+  darkO90 = "dark-o-90",
+  darkO75 = "dark-o-75",
   light = "light",
+  lightO90 = "light-o-90",
+  lightO75 = "light-o-75",
+  lightO50 = "light-o-50",
   psik = "psik",
   highbs = "highbs",
 }
 
 export default COLORS;
+
+export type BaseTextColors = COLORS.darkO90 | COLORS.lightO90;
